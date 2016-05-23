b722ef95bcbc89e54c1af92eb0be6455cc04a28c
summer <945501071@qq.com>
2016-05-23 06:28:10 -0700

    post index

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+
+def Index(request):
+    posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+
 
 # Create your views here.
